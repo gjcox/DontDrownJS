05ef6d37d6bfa510f0b97c600b3bfabf6b69ca55
Gabriel <gabrielcoxomega@gmail.com>
2023-11-03 13:36:35 +0000

    Removed random shapes; refactored drawing

--- dont-drown-app/src/Sketch.jsx
+++ dont-drown-app/src/Sketch.jsx
@@ -23,96 +23,16 @@ function determineSizes(width, height) {
 }
 
 function sketch(p5) {
+    var propped = undefined;
     const canvasScale = 0.8; // the proportion of the window to take up 
     const canvasDimensions = () => determineSizes(p5.windowWidth, p5.windowHeight).map(x => x * canvasScale);
     const background = 'lightgoldenrodyellow';
 
-    var centre; 
+    var centre;
     var sketcher;
-    var sketchedLine,
-        dualWeightedLine,
-        sketchedQuad,
-        sketchedRect,
-        sketchedEllipse,
-        sketchedWave;
     var pc;
     var crashDummy;
     const platforms = [];
-    var propped = undefined;
-
-    function sketchObjects() {
-        const framesPerRebuild = 30;
-        const rebuild = () => p5.frameCount % framesPerRebuild == 0;
-
-        if (!sketchedLine || rebuild()) {
-            sketchedLine = sketcher.buildSketchedLine(
-                p5.createVector(50, 100),
-                p5.createVector(150, 200),
-                'blue',
-                20
-            );
-        }
-
-        if (!dualWeightedLine || rebuild()) {
-            dualWeightedLine = sketcher.buildDualWeightedLine(
-                p5.createVector(150, 200),
-                p5.createVector(250, 250),
-                20,
-                50
-            );
-        }
-
-        if (!sketchedQuad || rebuild()) {
-            sketchedQuad = sketcher.buildSketchedQuad(
-                'gold',
-                'yellow',
-                [p5.createVector(300, 300),
-                p5.createVector(400, 300),
-                p5.createVector(385, 350),
-                p5.createVector(315, 350)],
-                10
-            );
-        }
-
-        if (!sketchedRect || rebuild()) {
-            sketchedRect = sketcher.buildSketchedRect(
-                'salmon',
-                'lightcyan',
-                400,
-                400,
-                75,
-                45,
-                10
-            );
-        }
-
-        if (!sketchedEllipse || rebuild()) {
-            sketchedEllipse = sketcher.buildSketchedEllipse(
-                'peru',
-                'palegreen',
-                550,
-                300,
-                150,
-                350,
-                30,
-                10
-            );
-        }
-
-        if (!sketchedWave || rebuild()) {
-            sketchedWave = sketcher.buildSketchedWave(
-                'lightskyblue',
-                'dodgerblue',
-                p5.width,
-                p5.height,
-                20,
-                10,
-                3,
-                p5.frameCount / framesPerRebuild,
-                10
-            );
-        }
-    }
 
     function handleKeyboardInput() {
         if ((p5.keyIsDown(p5.LEFT_ARROW) || p5.keyIsDown(65))
@@ -142,18 +62,6 @@ function sketch(p5) {
         }
     }
 
-    function drawObjects() {
-        sketchedLine.draw();
-        p5.push();
-        p5.fill('pink');
-        dualWeightedLine.draw();
-        p5.pop();
-        sketchedQuad.draw();
-        sketchedRect.draw();
-        sketchedEllipse.draw();
-        sketchedWave.draw({ x: 0, y: 500 });
-    }
-
     function randomPlatforms(nPlatforms) {
         for (let i = 0; i < nPlatforms; i++) {
             platforms.push(new Platform(p5, sketcher,
@@ -167,7 +75,7 @@ function sketch(p5) {
 
     p5.setup = () => {
         p5.createCanvas(...canvasDimensions());
-        centre = p5.createVector(p5.width / 2, p5.height / 2); 
+        centre = p5.createVector(p5.width / 2, p5.height / 2);
         sketcher = new Sketcher(p5);
         sketcher.lineBreaksMax = 2;
         sketcher.lineDeviationMult = 0.6;
@@ -185,6 +93,23 @@ function sketch(p5) {
         }
     };
 
+    function drawJumpRange() {
+        p5.push();
+        p5.stroke('black');
+        p5.line(pc.pos.x, pc.pos.y, pc.pos.x, pc.pos.y - (crashDummy.jumpHeight * pc.increment()));
+        p5.pop();
+    }
+
+    function drawDebugOverlay() {
+        p5.push();
+        p5.fill('black');
+        p5.textAlign(p5.RIGHT);
+        p5.textSize(100);
+        p5.text(`${Math.round(pc.velocity.x)}, ${Math.round(pc.velocity.y)}`,
+            p5.width / 10 * 9, p5.height / 10);
+        p5.pop();
+    }
+
     p5.draw = () => {
         /* React strict-mode calls useEffect twice, which means two 
          * canvases are sometimes generated. Seemingly when this happens 
@@ -193,8 +118,6 @@ function sketch(p5) {
         if (propped === undefined) { p5.remove(); return; };
 
         handleKeyboardInput();
-        // sketchObjects();
-        // drawObjects();
 
         // physics calculations 
         pc.integrate();
@@ -205,18 +128,8 @@ function sketch(p5) {
         p5.background(background);
         platforms.forEach(p => p.draw());
         pc.draw();
-        p5.push();
-        p5.stroke('black');
-        p5.line(pc.pos.x, pc.pos.y, pc.pos.x, pc.pos.y - (crashDummy.jumpHeight * pc.increment()));
-        p5.pop(); 
+        drawJumpRange();
         crashDummy.draw();
-        p5.push();
-        p5.fill('black');
-        p5.textAlign(p5.RIGHT);
-        p5.textSize(100);
-        p5.text(`${Math.round(pc.velocity.x)}, ${Math.round(pc.velocity.y)}`,
-            p5.width / 10 * 9, p5.height / 10);
-        p5.pop();
     };
 
     p5.windowResized = () => {
