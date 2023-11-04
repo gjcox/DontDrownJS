4f8da38f8d1e622c01b910063da6738e7714c8ae
Gabriel <gabrielcoxomega@gmail.com>
2023-11-04 22:47:34 +0000

    Implemented basic Wave class

---
+++ dont-drown-app/src/p5_modules/wave.js
@@ -0,0 +1,45 @@
+const WAVE_FOAM_COLOUR = 'lightskyblue';
+const WAVE_WATER_COLOUR = 'mediumblue';
+const N_CRESTS = 12; 
+const CREST_HEIGHT_DIV = 20; 
+const DETAIL = 3; 
+
+export default class Wave {
+    constructor(p5, sketcher) {
+        this.p5 = p5;
+        this._width = p5.width;
+        this._height = p5.width;
+        this.sprite = sketcher.buildSketchedWave(
+            WAVE_FOAM_COLOUR,
+            WAVE_WATER_COLOUR,
+            p5.width,
+            p5.height,
+            N_CRESTS,
+            p5.width / CREST_HEIGHT_DIV,
+            DETAIL,
+            0
+        );        
+        this._pos = p5.createVector(0, p5.height);
+    }
+
+    get pos() {
+        return this._pos; 
+    }
+
+    /**
+     * @param {p5.Vector} newPos
+     */
+    set pos(newPos) {
+        this._pos = newPos; 
+    }
+
+    onScreen() {
+        return this.pos.y <= this.p5.height && this.pos.y >= this.height;
+    }
+
+    draw() {
+        if (this.onScreen()) {
+            this.sprite.draw(this.pos);
+        }
+    }
+}
