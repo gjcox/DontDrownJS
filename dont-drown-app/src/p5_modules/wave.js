d4b6a5eca3f081dc21176024ea599f509a4980aa
Gabriel <gabrielcoxomega@gmail.com>
2023-11-21 10:20:23 +0000

    Tweaked Wave's sprite parameters

--- dont-drown-app/src/p5_modules/wave.js
+++ dont-drown-app/src/p5_modules/wave.js
@@ -1,23 +1,26 @@
 const WAVE_FOAM_COLOUR = 'lightskyblue';
 const WAVE_WATER_COLOUR = 'mediumblue';
 const N_CRESTS = 12; 
-const CREST_HEIGHT_DIV = 20; 
-const DETAIL = 3; 
+const CREST_HEIGHT_DIV = 50; 
+const DETAIL = 5; 
 
 export default class Wave {
     constructor(p5, sketcher) {
         this.p5 = p5;
         this._width = p5.width;
         this._height = p5.width;
+        this._crestHeight = p5.height / CREST_HEIGHT_DIV; 
+        const foamWeight = sketcher.defLineWeight * 3; 
         this.sprite = sketcher.buildSketchedWave(
             WAVE_FOAM_COLOUR,
             WAVE_WATER_COLOUR,
             p5.width,
             p5.height,
             N_CRESTS,
-            p5.width / CREST_HEIGHT_DIV,
+            this._crestHeight,
             DETAIL,
-            0
+            0,
+            foamWeight
         );        
         this._pos = p5.createVector(0, p5.height);
     }
@@ -38,7 +41,7 @@ export default class Wave {
     }
 
     onScreen() {
-        return this.pos.y <= this.p5.height;
+        return this.pos.y - this._crestHeight <= this.p5.height;
     }
 
     draw() {
