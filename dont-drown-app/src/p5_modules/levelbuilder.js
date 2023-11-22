a49c6559cda86f4cf767c9b10fb69f9f0f09eef1
Gabriel <gabrielcoxomega@gmail.com>
2023-11-22 13:32:07 +0000

    Reimplemented level generation; now top-down

--- dont-drown-app/src/p5_modules/levelbuilder.js
+++ dont-drown-app/src/p5_modules/levelbuilder.js
@@ -1,20 +1,28 @@
 import Level from "./level";
 import { marginX as calcMarginX } from "./page";
 import { increment } from "./physicsengine";
-import Platform, { PF_WIDTH_DIV } from "./platform";
+import Platform from "./platform";
 import { PC_DIAMETER_DIV } from "./playerball";
 
-// horizontal and vertical jump modifiers 
-const H_MIN_JUMP_RANGE_MULT = 0.6;
-const H_MAX_JUMP_RANGE_MULT = 0.9;
-const V_MIN_JUMP_RANGE_MULT = 0;
-const V_MAX_JUMP_RANGE_MULT = 0.25;
-const H_MIN_JUMP_HEIGHT_MULT = 0.25;
-const H_MAX_JUMP_HEIGHT_MULT = 0.5;
-const V_MIN_JUMP_HEIGHT_MULT = 0.75;
-const V_MAX_JUMP_HEIGHT_MULT = 1;
+// horizontal, vertical and reflection jump bounds 
+const H_JUMP_MIN_X_MULT = 0.6;
+const H_JUMP_MAX_X_MULT = 0.9;
+const H_JUMP_MIN_Y_MULT = 0.25;
+const H_JUMP_MAX_Y_MULT = 0.5;
+const V_JUMP_MIN_X_MULT = 0;
+const V_JUMP_MAX_X_MULT = 0.25;
+const V_JUMP_MIN_Y_MULT = 0.75;
+const V_JUMP_MAX_Y_MULT = 1;
+const R_JUMP_MIN_X_MULT = 0.25;
+const R_JUMP_MAX_X_MULT = 0.35;
+const R_JUMP_MIN_Y_MULT = 0.35;
+const R_JUMP_MAX_Y_MULT = 0.55;
 
 const DIRECTION_CHANGE_CHANCE = 0.1
+
+const H_JUMP = 101; // horizontal jump code 
+const V_JUMP = 102; // vertical jump code
+const R_JUMP = 103; // reflection jump code 
 
 export default class LevelBuilder {
     constructor(p5, sketcher, jumpHeight, jumpWidth) {
@@ -43,84 +51,84 @@ export default class LevelBuilder {
         return this.p5.createVector(x, y);
     }
 
+    randomJump(jumpType) {
+        let x, y; // displacements between current and next platform 
+
+        switch (jumpType) {
+            case H_JUMP:
+                x = this.jumpWidth * this.p5.random(H_JUMP_MIN_X_MULT, H_JUMP_MAX_X_MULT);
+                y = this.jumpHeight * this.p5.random(H_JUMP_MIN_Y_MULT, H_JUMP_MAX_Y_MULT);
+                break;
+            case R_JUMP:
+                x = this.jumpWidth * this.p5.random(R_JUMP_MIN_X_MULT, R_JUMP_MAX_X_MULT);
+                y = this.jumpHeight * this.p5.random(R_JUMP_MIN_Y_MULT, R_JUMP_MAX_Y_MULT);
+                break;
+            case V_JUMP:
+                x = this.jumpWidth * this.p5.random(V_JUMP_MIN_X_MULT, V_JUMP_MAX_X_MULT);
+                y = this.jumpHeight * this.p5.random(V_JUMP_MIN_Y_MULT, V_JUMP_MAX_Y_MULT);
+                break;
+            default:
+                throw new Error(`Unrecognised jumpType '${jumpType}' in randomJump()`);
+        }
+        return [x, y].map(x => x * increment(this.p5));
+    }
+
+
     buildLevel(difficulty) {
+        const width = this.p5.width;
         const levelHeight = difficulty.heightMult * this.p5.height;
-        const level = new Level(this.p5, difficulty);
-        const platforms = [];
-        const marginX = calcMarginX(this.p5)
-
-        // level generation values
-        const lowestPlatformHeight = 0.75 * this.p5.height;
+        const lowestPlatformHeight = 0.5 * this.p5.height;
         const highestPlatformHeight = this.p5.height + this.jumpHeight - levelHeight;
-        const playableWidth = this.p5.width - marginX;
+        const marginX = calcMarginX(this.p5)
+        const middlePlayable = (marginX + width) / 2;
+        const platformWidth = Platform.defaultWidth(this.p5);
 
-        var currentPlatform;
-
-        if (difficulty.hasGround) {
-            // TODO wide platforms 
-            currentPlatform = new Platform(this.p5, this.sketcher, this.p5.createVector(marginX, lowestPlatformHeight));
-            platforms.push(currentPlatform);
-        } else {
-            currentPlatform = new Platform(this.p5, this.sketcher,
-                this.p5.createVector(marginX + Math.random() * (playableWidth - this.p5.width / PF_WIDTH_DIV),
-                    lowestPlatformHeight));
-            platforms.push(currentPlatform);
+        function applyBounds(platformPos) {
+            platformPos.x = Math.max(marginX, platformPos.x);
+            platformPos.x = Math.min(platformPos.x, width - platformWidth);
         }
-        var highestPlatform = currentPlatform;
 
-        var diffX, diffY = 0; // displacements between current and next platform
-        var goingLeft = false;
+        const platforms = [];
+        const topPlatformPos = this.p5.createVector(
+            this.p5.random(marginX, width - platformWidth),
+            highestPlatformHeight
+        );
+        const topPlatform = new Platform(this.p5, this.sketcher, topPlatformPos);
+        platforms.push(topPlatform);
 
-        /* while current platform is at least a jump away from the max platform height */
-        while (currentPlatform.pos.y > highestPlatformHeight) {
+        const lastPlatform = () => platforms[platforms.length - 1];
 
-            const nextPlatform = new Platform(this.p5, this.sketcher, this.p5.createVector(0, 0));
-            const wentUp = diffY >= this.jumpHeight * V_MIN_JUMP_HEIGHT_MULT;
-            const edgeReached = currentPlatform.pos.x < marginX + nextPlatform.width
-                || currentPlatform.pos.x > this.p5.width - currentPlatform.width - nextPlatform.width;
+        // start with a horizontal jump...
+        var jumpType = H_JUMP;
+        // ... towards the horizontal middle of the playable area
+        var goingLeft = lastPlatform().middle.x > middlePlayable;
 
-            if (edgeReached) {
-                // turn around
+        do {
+            var nextPlatformPos = lastPlatform().pos.copy();
+            const [x, y] = this.randomJump(jumpType); // displacements between current and next platform 
+            nextPlatformPos.add(goingLeft ? -x : x, y);
+            applyBounds(nextPlatformPos);
+            platforms.push(new Platform(this.p5, this.sketcher, nextPlatformPos));
+
+            if (lastPlatform().pos.x < marginX + platformWidth
+                || lastPlatform().pos.x > width - 2 * platformWidth) {
+                // edge reached - reflection jump 
+                jumpType = R_JUMP;
                 goingLeft = !goingLeft;
-
-                // reflection jump
-                diffY = this.jumpHeight * this.p5.random(V_MIN_JUMP_HEIGHT_MULT, V_MAX_JUMP_HEIGHT_MULT);
-                diffX = Math.max(currentPlatform.width,
-                    this.jumpWidth * this.p5.random(H_MIN_JUMP_RANGE_MULT, V_MAX_JUMP_RANGE_MULT));
+            } else if (jumpType == V_JUMP || Math.random() >= difficulty.verticality) {
+                /* There cannot be two vertical jumps in a row,
+                   and vertical jumps' frequency is based on difficulty */
+                jumpType = H_JUMP;
             } else {
-                // random chance to change horizontal direction
-                if (Math.random() <= DIRECTION_CHANGE_CHANCE) {
-                    goingLeft = !goingLeft;
-                }
-
-                if (!wentUp && Math.random() < difficulty.verticality) {
-                    // vertical jump (can't have two in a row)
-                    diffY = this.jumpHeight * this.p5.random(V_MIN_JUMP_HEIGHT_MULT, V_MAX_JUMP_HEIGHT_MULT);
-                    diffX = this.jumpWidth * this.p5.random(V_MIN_JUMP_RANGE_MULT, V_MAX_JUMP_RANGE_MULT);
-                } else {
-                    // horizontal jump
-                    diffY = this.jumpHeight * this.p5.random(H_MIN_JUMP_HEIGHT_MULT, H_MAX_JUMP_HEIGHT_MULT);
-                    diffX = Math.max(currentPlatform.width,
-                        this.jumpWidth * this.p5.random(H_MIN_JUMP_RANGE_MULT, H_MAX_JUMP_RANGE_MULT));
-                }
-
+                jumpType = V_JUMP;
             }
 
-            if (goingLeft) {
-                diffX = -diffX;
+            // random chance to change direction if not a reflection jump 
+            if (jumpType != R_JUMP && Math.random() <= DIRECTION_CHANGE_CHANCE) {
+                goingLeft = !goingLeft;
             }
 
-            [diffX, diffY] = [diffX, diffY].map(x => x * increment(this.p5));
-            const platformPosition = this.placePlatform(marginX, currentPlatform, nextPlatform, diffX, diffY, highestPlatformHeight);
-            nextPlatform.initPos = platformPosition;
-            nextPlatform.pos = platformPosition;
-            currentPlatform = nextPlatform;
-            platforms.push(currentPlatform);
-
-            if (currentPlatform.pos.y < highestPlatform.pos.y) {
-                highestPlatform = currentPlatform;
-            }
-        }
+        } while (lastPlatform().pos.y < lowestPlatformHeight)
 
         // TODO colour top platform 
         // // replace the highest platform with a specially coloured one
@@ -128,6 +136,7 @@ export default class LevelBuilder {
         // platforms.remove(platforms.size() - 1);
         // platforms.add(highestPlatform);
 
+        const level = new Level(this.p5, difficulty);
         level.platforms = platforms;
         return level;
     }
