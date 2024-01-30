40aa661dbd12d031ab2a983283283b55122b1f49
Gabriel <gabrielcoxomega@gmail.com>
2024-01-30 18:09:47 +0000

    Line gaps now arguments to renderPage()

--- dont-drown-app/src/p5_modules/page.js
+++ dont-drown-app/src/p5_modules/page.js
@@ -1,26 +1,9 @@
-import Menu from "./menu";
-
 const PAGE_COLOUR = 'lightgoldenrodyellow';
 const LINE_WEIGHT = 1;
 const LINE_COLOUR = 'gray';
 const MARGIN_COLOUR = 'firebrick';
 
-function lineGap() {
-    const el = document.getElementById(Menu.gameMenuID());
-    const fontSize = parseInt(window.getComputedStyle(el).fontSize);
-    return fontSize;
-}
-
-function topLineGap() {
-    const titleEl = document.getElementById(Menu.titleID());
-    const titleFontSize = parseInt(window.getComputedStyle(titleEl).fontSize);
-    return 1.5 * titleFontSize;
-}
-
-function renderPage(p5, virtualTop, marginX) {
-    const _lineGap = lineGap();
-    const _topLineGap = topLineGap();
-
+function renderPage(p5, marginX, lineGap, topLineGap, virtualTop = 0) {
     // render page colour 
     p5.background(PAGE_COLOUR);
 
@@ -28,13 +11,13 @@ function renderPage(p5, virtualTop, marginX) {
     p5.push();
     p5.stroke(LINE_COLOUR);
     p5.strokeWeight(LINE_WEIGHT);
-    const topLineY = Math.abs(virtualTop) <= _topLineGap ?
-        virtualTop + _topLineGap :
-        _lineGap - (Math.abs(virtualTop) - _topLineGap) % _lineGap;
+    const topLineY = Math.abs(virtualTop) <= topLineGap ?
+        virtualTop + topLineGap :
+        lineGap - (Math.abs(virtualTop) - topLineGap) % lineGap;
     var lineY = topLineY;
     while (lineY <= p5.height) {
         p5.line(0, lineY, p5.width, lineY);
-        lineY += _lineGap;
+        lineY += lineGap;
     }
     p5.pop();
 
@@ -46,5 +29,5 @@ function renderPage(p5, virtualTop, marginX) {
     p5.pop();
 }
 
-export { LINE_WEIGHT, lineGap, renderPage, topLineGap };
+export { LINE_WEIGHT, renderPage };
 
