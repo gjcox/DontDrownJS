29bb2c563eac7a072de94dc790a1bd50544b6dd0
Gabriel <gabrielcoxomega@gmail.com>
2024-01-30 17:38:45 +0000

    Corrected BackButton's inline style syntax

--- dont-drown-app/src/components/BackButton.jsx
+++ dont-drown-app/src/components/BackButton.jsx
@@ -1,7 +1,7 @@
-export default BackButton = ({ menuHistory, goBack }) => {
+export default function ({ menuHistory, goBack }) {
     return <button
         className="menu-text menu-item-button"
-        style={"display: " + menuHistory.length == 0 ? "none" : "flex"}
+        style={{display: menuHistory.length == 0 ? "none" : "flex"}}
         onClick={() => goBack()}
     >
         ← BACK
