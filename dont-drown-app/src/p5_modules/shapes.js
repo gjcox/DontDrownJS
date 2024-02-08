0e03c9c35e8f179a742505f2ff3ecff295f19d07
Gabriel <gabrielcoxomega@gmail.com>
2024-02-08 14:55:01 +0000

    Refactored shape classes to use private properties

--- dont-drown-app/src/p5_modules/shapes.js
+++ dont-drown-app/src/p5_modules/shapes.js
@@ -1,61 +1,77 @@
 class Shape {
+  #p5;
+  #vertices;
+  #colour; 
+
   constructor(p5, colour) {
-    this.p5 = p5;
-    this.vertices = [];
-    this._colour = colour;
+    this.#p5 = p5;
+    this.#vertices = [];
+    this.#colour = colour;
   }
 
+  /**
+   * @param {any} colour
+   */
   set colour(colour) {
-    this._colour = colour;
+    this.#colour = colour;
   }
 
   addVertex(v = { x: 0, y: 0 }) {
-    this.vertices.push(v);
+    this.#vertices.push(v);
   }
 
   draw(pos = { x: 0, y: 0 }) {
-    this.p5.push();
-    if (this._colour) {
-      this.p5.fill(this._colour);
+    this.#p5.push();
+    if (this.#colour) {
+      this.#p5.fill(this.#colour);
     }
-    this.p5.beginShape();
-    this.vertices.forEach(({ x, y }) => this.p5.vertex(x + pos.x, y + pos.y));
-    this.p5.endShape(this.p5.CLOSE);
-    this.p5.pop();
+    this.#p5.beginShape();
+    this.#vertices.forEach(({ x, y }) => this.#p5.vertex(x + pos.x, y + pos.y));
+    this.#p5.endShape(this.#p5.CLOSE);
+    this.#p5.pop();
   }
 }
 
 class Ellipse {
+  #p5;
+  #x;
+  #y;
+  #width;
+  #height;
+  #colour; 
+
   constructor(p5, colour, x, y, w, h) {
-    this.p5 = p5;
-    this._colour = colour;
-    this.x = x;
-    this.y = y;
-    this.w = w;
-    this.h = h;
+    this.#p5 = p5;
+    this.#colour = colour;
+    this.#x = x;
+    this.#y = y;
+    this.#width = w;
+    this.#height = h;
   }
 
   draw(pos = { x: 0, y: 0 }) {
-    this.p5.push();
-    if (this._colour) {
-      this.p5.fill(this._colour);
+    this.#p5.push();
+    if (this.#colour) {
+      this.#p5.fill(this.#colour);
     }
-    this.p5.ellipse(this.x + pos.x, this.y + pos.y, this.w, this.h);
-    this.p5.pop();
+    this.#p5.ellipse(this.#x + pos.x, this.#y + pos.y, this.#width, this.#height);
+    this.#p5.pop();
   }
 }
 
 class CompositeShape {
+  #shapes; 
+
   constructor() {
-    this.shapes = [];
+    this.#shapes = [];
   }
 
   addShape(s) {
-    this.shapes.push(s);
+    this.#shapes.push(s);
   }
 
   draw(pos = { x: 0, y: 0 }) {
-    this.shapes.forEach((s) => s.draw(pos));
+    this.#shapes.forEach((s) => s.draw(pos));
   }
 }
 
