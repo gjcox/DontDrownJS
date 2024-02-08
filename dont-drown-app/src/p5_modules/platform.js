ba80d09e967e7386973aab0f35465d901efb6a61
Gabriel <gabrielcoxomega@gmail.com>
2024-02-08 14:39:26 +0000

    Refactored Platform to use private properties

--- dont-drown-app/src/p5_modules/platform.js
+++ dont-drown-app/src/p5_modules/platform.js
@@ -4,74 +4,73 @@ export default class Platform {
     static #PF_HEIGHT_DIV = 40; // relative to canvas width
 
     static get widthDiv() {
-        return Platform.#PF_WIDTH_DIV; 
+        return Platform.#PF_WIDTH_DIV;
     }
 
     static defaultWidth(p5) {
-        return p5.width / Platform.#PF_WIDTH_DIV; 
+        return p5.width / Platform.#PF_WIDTH_DIV;
     }
 
     static get heightDiv() {
-        return Platform.#PF_HEIGHT_DIV; 
+        return Platform.#PF_HEIGHT_DIV;
     }
 
     static defaultHeight(p5) {
-        return p5.width / Platform.#PF_HEIGHT_DIV; 
+        return p5.width / Platform.#PF_HEIGHT_DIV;
     }
 
+    #p5;
+    #width;
+    #height;
+    #pos;
+    #initPos;
+
     constructor(p5, pos) {
-        this.p5 = p5;
-        this._width = Platform.defaultWidth(p5);
-        this._height = Platform.defaultHeight(p5);
-        this._pos = pos.copy();
-        this._initPos = pos.copy();
+        this.#p5 = p5;
+        this.#width = Platform.defaultWidth(p5);
+        this.#height = Platform.defaultHeight(p5);
+        this.#pos = pos.copy();
+        this.#initPos = pos.copy();
     }
 
     get width() {
-        return this._width;
+        return this.#width;
     }
 
     get height() {
-        return this._height;
+        return this.#height;
     }
 
     get pos() {
-        return this._pos.copy();
+        return this.#pos.copy();
     }
 
     /**
      * @param {p5.Vector} newPos 
      */
     set pos(newPos) {
-        this._pos = newPos.copy();
+        this.#pos = newPos.copy();
     }
 
     get initPos() {
-        return this._initPos.copy();
-    }
-
-    /**
-     * @param {p5.Vector} newPos 
-     */
-    set initPos(newPos) {
-        this._initPos = newPos.copy();
+        return this.#initPos.copy();
     }
 
     get middle() {
-        return this.pos.copy().add(this.width / 2, 0);
+        return this.#pos.copy().add(this.#width / 2, 0);
     }
 
     translate(v) {
-        this._pos.add(v); 
+        this.#pos.add(v);
     }
 
     onScreen() {
-        return this.pos.y <= this.p5.height && this.pos.y >= this.height;
+        return this.#pos.y <= this.#p5.height && this.#pos.y >= this.#height;
     }
 
     draw() {
         if (this.onScreen()) {
-            this.sprite.draw(this._pos);
+            this.sprite.draw(this.#pos);
         }
     }
 }
